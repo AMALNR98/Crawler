2b5b1382a4951f953c5180d913866b1e8b3e9fdb
AMALNR98 <amalnr123@gmail.com>
2022-08-16 11:10:25 +0530

    main function created

--- crawler.py
+++ crawler.py
@@ -9,3 +9,23 @@ def parse_args():
     parser.add_argument(
         "-d", "--debug", help="Enable debug logging", bs4action="store_true")
     return parser.parse_args()
+
+def main():
+    # args = parse_args()
+    # artist = get_artists_name()
+    # songs = get_songs_name()
+    # lyrics = get_lyrics()
+    # if args.debug:
+    #     configure_logging(logging.DEBUG)
+    # else:
+    #     configure_logging(logging.INFO)
+    # logger.debug("Here's a debug message")
+    # logger.info("Here's an info message!")
+    # logger.warning("Here's an warning message!")
+    # logger.critical("Here's an critical message!")
+    # crawl()
+if __name__ == "__main__":
+    main()
+# artist
+# songs
+# lyrics
